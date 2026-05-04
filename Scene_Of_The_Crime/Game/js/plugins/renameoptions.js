0bd45d14070262aa1f28ad67e6e3ea572dd987d1
Sumi Yi <sumiyi1015@gmail.com>
2026-05-04 15:26:38 -0700

    Rename options to audio in title

--- Scene_Of_The_Crime/Game/js/plugins/renameoptions.js
+++ Scene_Of_The_Crime/Game/js/plugins/renameoptions.js
@@ -1,4 +1,16 @@
 (() => {
+    const _Window_TitleCommand_makeCommandList =
+        Window_TitleCommand.prototype.makeCommandList;
+
+    Window_TitleCommand.prototype.makeCommandList = function() {
+        _Window_TitleCommand_makeCommandList.call(this);
+
+        const command = this._list.find(cmd => cmd.symbol === "options");
+        if (command) {
+            command.name = "Audio";
+        }
+    };
+
     Window_MenuCommand.prototype.addOptionsCommand = function() {
         this.addCommand("Audio", "options", true);
     };
